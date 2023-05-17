bb4e13779e186ca0fad69a1e8f8fa993078e86fb
LeJuanTenorio <tenorio2003@gmail.com>
2023-05-16 20:52:58 -0500

    Commit 5

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,10 +7,10 @@ const carrusel = document.querySelector(".carrusel");
 
 const myFetchCarrusel = async() => {
 
-  const response = await fetch("");
+  const response = await fetch("https://raw.githubusercontent.com/LeJuanTenorio/Entrega-2-Star-Wars/main/Assets/json/jsonCarrusel.json");
   const data = await response.json();
 
-  for (let item of sum) {
+  for (let item of data) {
     let imgObj = new RenderCarrusel(item.image);
     const imgRender = imgObj.render();
     carrusel.appendChild(imgRender);
