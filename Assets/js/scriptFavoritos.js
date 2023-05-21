09ea20c5035abbf7db4ece9e357864efd824b6bb
LeJuanTenorio <tenorio2003@gmail.com>
2023-05-21 09:00:01 -0500

    commit 9000

--- Assets/js/scriptFavoritos.js
+++ Assets/js/scriptFavoritos.js
@@ -1,33 +1,30 @@
-//const renderIframe = () => {
-//    const reset = document.getElementsByClassName("iframeContainer")
-//    reset.remove();
-//
-  //  const iframe = document.createElement('div');
-  //  iframe.className = "iframeContainer";
- //   
-  //  const iframeUrl = document.createElement('iframe');
- //   iframeUrl.src = url;
-   // iframeContainer.appendChild(iframeUrl);
+const renderIframe = () => {
+    const reset = document.getElementsByClassName("iframeContainer")
+    reset.remove();
 
-  //  body.appendChild(iframeContainer);
-//} 
+    const iframe = document.createElement('div');
+    iframe.className = "iframeContainer";
+    
+    const iframeUrl = document.createElement('iframe');
+    iframeUrl.src = url;
+    iframeContainer.appendChild(iframeUrl);
 
-//const modalIframe = () => {
-//modal.style.display = "block";
-//}//
+    body.appendChild(iframeContainer);
+} 
 
-//const modalClose = (noModal) => {
-//if(noModal.target == modal){
-//    modal.style.display = "none";
-//}
-//}
+const modalIframe = () => {
+modal.style.display = "block";
+}
+
+const modalClose = (noModal) => {
+if(noModal.target == modal){
+    modal.style.display = "none";
+}
+}
 
 
 
-//peli.onclick = renderIframe;
-//const modal = document.getElementsByClassName("iframeContainer");
-//peli.onclick = modalIframe; 
-//window.onclick = modalClose;
+
 
 
 const CargarPeliculas = async() => {
@@ -35,16 +32,18 @@ const CargarPeliculas = async() => {
    console.log(respuesta);
 
    const peliculas = await respuesta.json();
+   console.log(peliculas)
 let peliculasas = ''; 
 peliculas.forEach((pelicula) => {
-  const imagenUrl = pelicula.poster;
+  const imagenUrl = pelicula.image;
   const imagenCompletaUrl = `https://vignette.wikia.nocookie.net/starwars/images/${imagenUrl}`;
   const urlWiki = `https://starwars.fandom.com/wiki/${pelicula.name}`;
   peliculasas = peliculasas + `
-    <div class="peli" id ="${urlWiki}">
-      <img class="poster" src="${imagenCompletaUrl}" >
+  <link rel="stylesheet" href="/estilos.css">
+  <div class="peli" id ="${urlWiki}">
+      <img class="image" src="${imagenUrl}">
     </div>
-    <h1>${pelicula.name}</h1>`; 
+    <h1 onclick="renderIframe(); modalIframe()">${pelicula.name}</h1>`; 
 });
 document.getElementById('contenedor').innerHTML = peliculasas;
 
